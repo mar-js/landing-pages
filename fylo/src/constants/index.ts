ac004c9e9ad74cb2b8a1115fd6dc972f31deb1f9
Martin <maguera37@gmail.com>
2024-07-08 22:05:00 -0300

    feat: create cardData constant

--- fylo/src/constants/index.ts
+++ fylo/src/constants/index.ts
@@ -1,3 +1,8 @@
+import iconAccess from "@images/icon-access.svg";
+import iconCollaboration from "@images/icon-collaboration.svg";
+import iconFile from "@images/icon-file.svg";
+import iconSecurity from "@images/icon-security.svg";
+
 export const menuHeader = (alternative: boolean) => [
 	alternative
 		? {
@@ -60,4 +65,35 @@ export const menuFooter = [
 			text: "Privacy",
 		},
 	],
+];
+
+export const cardData = [
+	{
+		id: 1,
+		icon: iconAccess,
+		title: "Access your files, anywhere",
+		description:
+			"The ability to use a smartphone, tablet, or computer to access your account means your files follow you everywhere.",
+	},
+	{
+		id: 2,
+		icon: iconSecurity,
+		title: "Security you can trust",
+		description:
+			"2-factor authentication and user-controlled encryption are just a couple of the security features we allow to help secure your files",
+	},
+	{
+		id: 3,
+		icon: iconCollaboration,
+		title: "Real-time collaboration",
+		description:
+			"Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required",
+	},
+	{
+		id: 4,
+		icon: iconFile,
+		title: "Store any type of file",
+		description:
+			"Whether you're sharing holidays photos or work documents, Fylo has you covered allowing for all file types to be securely stored and shared",
+	},
 ];
