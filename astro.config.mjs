cfedfe51f1abf417166bee74dfc477081dfbf054
Martin <maguera37@gmail.com>
2024-08-14 19:04:28 -0300

    change value in base key of defineConfig in astro.config file V2

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://mar-js.github.io/landing-pages/",
-	base: "https://mar-js.github.io/landing-pages/",
+	base: "landing-pages/",
 	integrations: [tailwind()],
 	prefetch: true,
 });
