b57964f4f96c246800e8603e58b9b1345d00cb07
Martin <maguera37@gmail.com>
2024-08-15 22:50:27 -0300

    create PAGES const in constants file

---
+++ src/constants/index.ts
@@ -0,0 +1,16 @@
+import fylo from "@assets/icons/fylo.svg";
+import huddle from "@assets/icons/huddle.svg";
+import type { TPages } from "@types";
+
+export const PAGES: TPages[] = [
+	{
+		id: "fylo",
+		name: "Fylo",
+		logo: fylo,
+	},
+	{
+		id: "huddle",
+		name: "Huddle",
+		logo: huddle,
+	},
+];
